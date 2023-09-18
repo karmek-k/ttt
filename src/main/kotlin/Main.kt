77bb616c84b4e3fc001cb7e6815c9090fd600917
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-09-18 21:47:57 +0200

    feat: add boardAsString function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Tic-Tac-Toe")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println(boardAsString())
+}
+
+fun boardAsString(): String {
+    return  "   |   |   " + "\n" +
+            "---.---.---" + "\n" +
+            "   |   |   " + "\n" +
+            "---.---.---" + "\n" +
+            "   |   |   "
 }
