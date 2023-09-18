0564204cb4cba86b40aaa563a90f2ad8329c7935
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-09-18 23:12:25 +0200

    feat: read players' symbols

--- src/main/kotlin/game/Main.kt
+++ src/main/kotlin/game/Main.kt
@@ -1,10 +1,43 @@
 package game
 
 import game.board.Board
+import game.board.CellState
+import java.lang.Exception
 
-fun main(args: Array<String>) {
+fun main() {
     println("Tic-Tac-Toe")
+    println()
+
+    val p1 = getPlayerSymbol("Player 1")
+    val p2 = getPlayerSymbol("Player 2")
 
     val board = Board()
     println(board)
+}
+
+fun getPlayerSymbol(playerName: String): CellState {
+    var symbol: String
+    var hasFailed = false
+
+    do {
+        if (hasFailed) {
+            println()
+            println("Invalid symbol!")
+        }
+
+        print("Select symbol for $playerName (x or o): ")
+        symbol = readln()
+
+        hasFailed = true
+    } while (!isSymbolValid(symbol))
+
+    return when (symbol) {
+        "x", "X" -> CellState.X
+        "o", "O" -> CellState.O
+        else -> throw Exception("this shouldn't happen")
+    }
+}
+
+fun isSymbolValid(symbol: String): Boolean {
+    return symbol == "x" || symbol == "X" || symbol == "o" || symbol == "O"
 }
